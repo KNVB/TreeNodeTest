e434f26887e4df35f6dc5741e287824594c17c50
HKO Helpdesk <helpdesk@hko.gov.hk>
2019-05-14 13:40:13 +0800

    Modify icon mapping

--- TreeNodeTest/Form1.Designer.cs
+++ TreeNodeTest/Form1.Designer.cs
@@ -74,19 +74,20 @@
             this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "0.bmp");
-            this.imageList1.Images.SetKeyName(1, "1.bmp");
-            this.imageList1.Images.SetKeyName(2, "2.bmp");
-            this.imageList1.Images.SetKeyName(3, "3.bmp");
-            this.imageList1.Images.SetKeyName(4, "8.bmp");
-            this.imageList1.Images.SetKeyName(5, "4.bmp");
-            this.imageList1.Images.SetKeyName(6, "5.bmp");
-            this.imageList1.Images.SetKeyName(7, "9.bmp");
-            this.imageList1.Images.SetKeyName(8, "Button Play.png");
-            this.imageList1.Images.SetKeyName(9, "Button Stop.png");
-            this.imageList1.Images.SetKeyName(10, "6.bmp");
-            this.imageList1.Images.SetKeyName(11, "7.bmp");
-            this.imageList1.Images.SetKeyName(12, "ftp-user-icon.png");
-            this.imageList1.Images.SetKeyName(13, "ftp-user-group-icon.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-edit-property-24.png");
+            this.imageList1.Images.SetKeyName(2, "1.bmp");
+            this.imageList1.Images.SetKeyName(3, "2.bmp");
+            this.imageList1.Images.SetKeyName(4, "3.bmp");
+            this.imageList1.Images.SetKeyName(5, "8.bmp");
+            this.imageList1.Images.SetKeyName(6, "4.bmp");
+            this.imageList1.Images.SetKeyName(7, "5.bmp");
+            this.imageList1.Images.SetKeyName(8, "9.bmp");
+            this.imageList1.Images.SetKeyName(9, "Button Play.png");
+            this.imageList1.Images.SetKeyName(10, "Button Stop.png");
+            this.imageList1.Images.SetKeyName(11, "6.bmp");
+            this.imageList1.Images.SetKeyName(12, "7.bmp");
+            this.imageList1.Images.SetKeyName(13, "ftp-user-icon.png");
+            this.imageList1.Images.SetKeyName(14, "ftp-user-group-icon.png");
             // 
             // listView1
             // 
